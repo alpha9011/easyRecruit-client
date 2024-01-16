4fd04bc007d7e317f4e79206f49471477073f6f0
sazzadchy50 <sazzadchy50@gmail.com>
2024-01-16 17:05:24 +0600

    flowbite react added

--- src/Page/Home/Banner/Banner.jsx
+++ src/Page/Home/Banner/Banner.jsx
@@ -1,14 +1,27 @@
-import bannerImg from "../../../assets/banner img.png"
+import { Button, FloatingLabel } from "flowbite-react";
+import bannerImg from "../../../assets/banner img.png";
 
 const Banner = () => {
-    return (
-        <div>
-            <div>
-                <img src={bannerImg} alt="" />
-            </div>
+  return (
+    <div className="container mx-auto relative border h-80vh" > 
+    <div className="flex justify-center text-5xl ">
+        <h1>Website title </h1>
+    </div>
 
-        </div>
-    );
+    <div className="">
+        <img src={bannerImg} alt="" />
+      </div>
+       
+      <div className="absolute  top-1/2 left-1/2 flex">
+        <input type="text" className=" " placeholder="text here " />
+
+        <Button outline className="rounded-none" gradientDuoTone="pinkToOrange">
+          SEARCH
+        </Button>
+      </div>
+      
+    </div>
+  );
 };
 
 export default Banner;
