cc3b17c596178d6e866d48547ed96047fa912bc1
sazzadchy50 <sazzadchy50@gmail.com>
2024-01-18 13:55:39 +0600

    How to use section added

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,7 +1,8 @@
-import Navbars from "../../shared/Navbars/Navbars";
+
 
 import SocialIcon from "../../shared/SocialIcon/SocialIcon";
 import Banner from "./Banner/Banner";
+import HowToUse from "./HowToUse/HowToUse";
 import LatestJobs from "./LatestJob/LatestJobs";
 import Reviews from "./Reviews/Reviews";
 
@@ -9,9 +10,10 @@ import Reviews from "./Reviews/Reviews";
 const Home = () => {
     return (
         <div>
-            <Navbars></Navbars>
-            <Banner />
+           
+            <Banner/>
             <SocialIcon></SocialIcon>
+            <HowToUse/>
             <Reviews></Reviews>
             <LatestJobs></LatestJobs>
         </div>
