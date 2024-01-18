a266b308acb12a1336b4e074d716b59aa253f01e
Rahim00001 <rahimlpt2022@gmail.com>
2024-01-18 17:37:13 +0600

    About Us page designed

--- src/Page/AboutUs/AboutUs.jsx
+++ src/Page/AboutUs/AboutUs.jsx
@@ -1,12 +1,14 @@
 import OurMission from "./OurMission";
+import Team from "./Team";
 import WhyUs from "./WhyUs";
 
 const AboutUs = () => {
     return (
         <div className="my-5">
-            <h2 className="text-4xl text-center font-semibold">About Us</h2>
+            <h2 className="text-6xl text-center font-bold my-10">About Us</h2>
             <OurMission></OurMission>
             <WhyUs></WhyUs>
+            <Team></Team>
         </div>
     );
 };
