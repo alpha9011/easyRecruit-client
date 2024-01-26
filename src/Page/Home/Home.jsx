1af4d0c5ab431bc2d65ae61112be9bb9072f361f
sazzadchy50 <sazzadchy50@gmail.com>
2024-01-26 12:15:34 +0600

    Primary section responsive issue solve

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -11,15 +11,7 @@ import PartnerProgram from "./PartnerProgram/PartnerProgram";
 import RecruitingResult from "./RecruitingResult/RecruitingResult";
 
 import WhyToUse from "./WhyToUse/WhyToUse";
-// import Footers from "../../shared/Footer/Footers";
-// import SocialIcon from "../../shared/SocialIcon/SocialIcon";
 
-// import PopularCategoriy from "./PopularCategoriy/PopularCategoriy";
-
-// import Countdown from "./Countdown/Countdown";
-// import HowToUse from "./HowToUse/HowToUse";
-// import LatestJobs from "./LatestJob/LatestJobs";
-// import Reviews from "./Reviews/Reviews";
 const Home = () => {
   return (
     <div>
@@ -31,23 +23,11 @@ const Home = () => {
       <PartnerProgram></PartnerProgram>
       <CustomersSlider></CustomersSlider>
       <MakingHiresBanner></MakingHiresBanner>
+      
     </div>
   );
 };
 
-// const Home = () => {
-//     return (
-//         <div>
-//             <Banner />
-//             <PopularCategoriy/>
-//             <SocialIcon></SocialIcon>
-//             <Reviews></Reviews>
-//             <LatestJobs></LatestJobs>
-//             <HowToUse></HowToUse>
-//             <Countdown></Countdown>
-//             <Footers></Footers>
-//         </div>
-//     );
-// };
+
 
 export default Home;
--- src/Page/Home/HrSystems/HrSystems.jsx
+++ src/Page/Home/HrSystems/HrSystems.jsx
@@ -1,5 +1,6 @@
 
-import hrSystem from "../../../../public/Image/HrSystem.jpg";
+import { Link } from "react-router-dom";
+import hrSystemImg from "../../../../public/Image/HrSystem.jpg";
 import PrimarySection from "../../../shared/PrimarySection/PrimarySection";
 
 const HrSystems = () => {
@@ -12,8 +13,9 @@ const HrSystems = () => {
   const linkUrl = "";
 
   return (
-    <div>
-      <PrimarySection
+    <div className="flex  flex-row-reverse">
+      
+      {/* <PrimarySection
         title={title}
         subTitle={subTitle}
         content={content}
@@ -22,7 +24,30 @@ const HrSystems = () => {
         image={hrSystem}
         alt={"Easy recruit"}
         imgPosition="right"
-      ></PrimarySection>
+      ></PrimarySection> */}
+   <div className="p-5 my-10 container mx-auto">
+      <div
+        className={` flex flex-col 
+        md:flex-row-reverse
+       justify-center items-center gap-10  `}
+      >   
+        <div className="md:w-1/2">
+          <img className="w-full md:w-96 lg:w-[700px]" src={hrSystemImg} alt={'easyRecruit'} />
+        </div>
+        <div className="md:w-1/2 flex flex-col space-y-3 max-w-[370px] justify-center ">
+          <h6 className="text-sky-500 font-semibold">{subTitle}</h6>
+          <h3 className="text-xl md:text-3xl font-semibold">{title}</h3>
+          <p className="text-wrap leading-7">{content}</p>
+
+          <Link
+            to={linkUrl}
+            className="text-sky-500 hover:underline text-wrap hover:font-semibold"
+          >
+            {linkText}
+          </Link>
+        </div>
+      </div>
+    </div>
     
     </div>
   );
