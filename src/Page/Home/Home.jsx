d5ff76ef64e2a7edd167efe9465de47d6cf6fdf2
redwan9011 <redwan390309@gmail.com>
2024-01-26 19:17:40 +0600

    changes the apply form

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,3 +1,4 @@
+import { Footer } from "flowbite-react";
 import Banner from "./Banner/Banner";
 
 import CustomersSlider from "./CustomersSlider/CustomersSlider";
@@ -11,15 +12,8 @@ import PartnerProgram from "./PartnerProgram/PartnerProgram";
 import RecruitingResult from "./RecruitingResult/RecruitingResult";
 
 import WhyToUse from "./WhyToUse/WhyToUse";
-// import Footers from "../../shared/Footer/Footers";
-// import SocialIcon from "../../shared/SocialIcon/SocialIcon";
+import Footers from "../../shared/Footer/Footers";
 
-// import PopularCategoriy from "./PopularCategoriy/PopularCategoriy";
-
-// import Countdown from "./Countdown/Countdown";
-// import HowToUse from "./HowToUse/HowToUse";
-// import LatestJobs from "./LatestJob/LatestJobs";
-// import Reviews from "./Reviews/Reviews";
 const Home = () => {
   return (
     <div>
@@ -31,23 +25,10 @@ const Home = () => {
       <PartnerProgram></PartnerProgram>
       <CustomersSlider></CustomersSlider>
       <MakingHiresBanner></MakingHiresBanner>
+      <Footers></Footers>
     </div>
   );
 };
 
-// const Home = () => {
-//     return (
-//         <div>
-//             <Banner />
-//             <PopularCategoriy/>
-//             <SocialIcon></SocialIcon>
-//             <Reviews></Reviews>
-//             <LatestJobs></LatestJobs>
-//             <HowToUse></HowToUse>
-//             <Countdown></Countdown>
-//             <Footers></Footers>
-//         </div>
-//     );
-// };
 
 export default Home;
