eea57a61b703291da14dfac707344af9739c46dd
aminul157246 <aminul.islam.157246@gmail.com>
2024-02-01 21:17:09 +0600

    trying to pull

--- src/Page/Register/Register.jsx
+++ src/Page/Register/Register.jsx
@@ -1,4 +1,4 @@
-import { Button, FileInput,  Label, Select, TextInput } from 'flowbite-react';
+import { Button, FileInput, Label, Select, TextInput } from 'flowbite-react';
 import { useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import 'tailwindcss/tailwind.css';
@@ -25,7 +25,7 @@ const Register = () => {
         const company = event.target.company.value;
         const PhoneNO = event.target.PhoneNO.value;
         const aboutUS = event.target.aboutUS.value;
-        console.log(firstName, lastName, email,password, company, PhoneNO,aboutUS);
+        console.log(firstName, lastName, email, password, company, PhoneNO, aboutUS);
 
 
 
@@ -57,7 +57,7 @@ const Register = () => {
         createUser(email, password)
             .then(res => {
                 console.log(res.user);
-                handleUpdateProfile(name, img)
+                handleUpdateProfile( img)
                     .then(() => {
                         Toastify({
                             text: "Register successful",
@@ -79,133 +79,130 @@ const Register = () => {
     return (
 
         <>
-        <div style={{
-            backgroundImage: `linear-gradient(to right, rgba(255, 107, 107, 0.5), rgba(255, 224, 102, 0.5)), url(${bgRegister})`,
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: 'cover',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-        }}
+            <div style={{
+                backgroundImage: `linear-gradient(to right, rgba(255, 107, 107, 0.5), rgba(255, 224, 102, 0.5)), url(${bgRegister})`,
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}
             >
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 p-8 md:p-10 lg:gap-16  lg:p-24'>
-            <div>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 p-8 md:p-10 lg:gap-16  lg:p-24'>
+                    <div>
 
-<div className='text-xl'>
-    <h1 className='text-5xl font-semibold mb-4'>Try EasyRecruit for Free <br /> Today</h1>
-    <p>Start a 14-day free trial today with unlimited users and no credit card required.*</p>
-    <h3>Post your first job for free today and see why over 10,000 growing businesses trust JazzHR to power their hiring process.</h3>
-    <ul className='mt-4'>
-        <li className='flex items-center gap-2'><FaRegCircle className='text-sm'/>Voted #1 most user-friendly applicant tracking system</li>
-        <li className='flex items-center gap-2'><FaRegCircle className='text-sm'/>Reduces time to hire by 50%</li>
-        <li className='flex items-center gap-2'><FaRegCircle className='text-sm'/>Creates a lasting candidate experience</li>
-        <li className='flex items-center gap-2'><FaRegCircle className='text-sm'/>Transparent pricing that is actually affordable</li>
-    </ul>
-</div>
+                        <div className='text-xl'>
+                            <h1 className='text-5xl font-semibold mb-4'>Try EasyRecruit for Free <br /> Today</h1>
+                            <p>Start a 14-day free trial today with unlimited users and no credit card required.*</p>
+                            <h3>Post your first job for free today and see why over 10,000 growing businesses trust JazzHR to power their hiring process.</h3>
+                            <ul className='mt-4'>
+                                <li className='flex items-center gap-2'><FaRegCircle className='text-sm' />Voted #1 most user-friendly applicant tracking system</li>
+                                <li className='flex items-center gap-2'><FaRegCircle className='text-sm' />Reduces time to hire by 50%</li>
+                                <li className='flex items-center gap-2'><FaRegCircle className='text-sm' />Creates a lasting candidate experience</li>
+                                <li className='flex items-center gap-2'><FaRegCircle className='text-sm' />Transparent pricing that is actually affordable</li>
+                            </ul>
+                        </div>
 
-<div className='mt-4' style={{fontStyle:'italic'}}>
-<p>Note: Indeed access is restricted during the trial experience. To access Indeed, please purchase a JazzHR plan.</p>
+                        <div className='mt-4' style={{ fontStyle: 'italic' }}>
+                            <p>Note: Indeed access is restricted during the trial experience. To access Indeed, please purchase a JazzHR plan.</p>
 
-<p className='mt-4'>*If you are looking for a job, we kindly request that you refrain from completing this form. The JazzHR free trial is exclusively available for businesses seeking to make new hires, and not intended for individual candidates. Thank you for your understanding</p>
-</div>
-</div>
+                            <p className='mt-4'>*If you are looking for a job, we kindly request that you refrain from completing this form. The JazzHR free trial is exclusively available for businesses seeking to make new hires, and not intended for individual candidates. Thank you for your understanding</p>
+                        </div>
+                    </div>
 
-<div className='bg-[#b9cdc6]  p-12  lg:w-[500px]'>
+                    <div className='bg-[#b9cdc6]  p-12  lg:w-[500px]'>
 
-<h2 className='text-2xl font-semibold mb-4 text-center'>Create a Free Account</h2>
-<form onSubmit={handleSubmit} className="flex max-w-md flex-col  gap-4">
+                        <h2 className='text-2xl font-semibold mb-4 text-center'>Create a Free Account</h2>
+                        <form onSubmit={handleSubmit} className="flex max-w-md flex-col  gap-4">
 
-    <div>
-        <div className="mb-2 block">
-            <Label htmlFor="firstName" value="*First name" />
-        </div>
-        <TextInput id="firstName" type="text" required shadow />
-    </div>
-
-
-    <div>
-        <div className="mb-2 block">
-            <Label htmlFor="lastName" value="*Last name" />
-        </div>
-        <TextInput id="lastName" type="text" required shadow />
-    </div>
-
-    <div>
-        <div className="mb-2 block">
-            <Label htmlFor="email" value="*Work email address" />
-        </div>
-        <TextInput id="email" type="email" placeholder="name123@gmail.com" required shadow />
-    </div>
+                            <div>
+                                <div className="mb-2 block">
+                                    <Label htmlFor="firstName" value="*First name" />
+                                </div>
+                                <TextInput id="firstName" type="text" required shadow />
+                            </div>
 
 
-    <div id="fileUpload" className="max-w-md">
-<div className="mb-2 block">
-<Label htmlFor="file" value="*Image" />
-</div>
-<FileInput id="file" helperText="" />
-</div>
+                            <div>
+                                <div className="mb-2 block">
+                                    <Label htmlFor="lastName" value="*Last name" />
+                                </div>
+                                <TextInput id="lastName" type="text" required shadow />
+                            </div>
 
-    <div>
-        <div className="mb-2 block">
-            <Label htmlFor="PhoneNO" value="*Phone Number" />
-        </div>
-        <TextInput id="PhoneNO" type="number" placeholder='1+XXX-XXX-XXX' required shadow />
-    </div>
-    <div>
-        <div className="mb-2 block">
-            <Label htmlFor="password" value="*Your password" />
-        </div>
-        <TextInput id="password" type="password" required shadow />
-    </div>
+                            <div>
+                                <div className="mb-2 block">
+                                    <Label htmlFor="email" value="*Work email address" />
+                                </div>
+                                <TextInput id="email" type="email" placeholder="name123@gmail.com" required shadow />
+                            </div>
 
 
+                            <div id="fileUpload" className="max-w-md">
+                                <div className="mb-2 block">
+                                    <Label htmlFor="file" value="*Image" />
+                                </div>
+                                <FileInput id="file" helperText="" />
+                            </div>
 
-    <div className="max-w-md">
-        <div className="mb-2 block">
-            <Label htmlFor="company" value="*Company " />
-        </div>
-        <TextInput id="company" type="text" required shadow />
-
-    </div>
+                            <div>
+                                <div className="mb-2 block">
+                                    <Label htmlFor="PhoneNO" value="*Phone Number" />
+                                </div>
+                                <TextInput id="PhoneNO" type="number" placeholder='1+XXX-XXX-XXX' required shadow />
+                            </div>
+                            <div>
+                                <div className="mb-2 block">
+                                    <Label htmlFor="password" value="*Your password" />
+                                </div>
+                                <TextInput id="password" type="password" required shadow />
+                            </div>
 
 
 
-    <div className="max-w-md">
-        <div className="mb-2 block">
-            <Label htmlFor="aboutUS" value="*How did you hear about us?:" />
-        </div>
-        <Select id="aboutUS" required>
-            <option>  Select...</option>
-            <option>Social Media</option>
-            <option>Advertising</option>
-            <option>Previous Customer</option>
-            <option>I am looking for a job</option>
+                            <div className="max-w-md">
+                                <div className="mb-2 block">
+                                    <Label htmlFor="company" value="*Company " />
+                                </div>
+                                <TextInput id="company" type="text" required shadow />
 
-        </Select>
-    </div>
+                            </div>
 
 
 
+                            <div className="max-w-md">
+                                <div className="mb-2 block">
+                                    <Label htmlFor="aboutUS" value="*How did you hear about us?:" />
+                                </div>
+                                <Select id="aboutUS" required>
+                                    <option>  Select...</option>
+                                    <option>Social Media</option>
+                                    <option>Advertising</option>
+                                    <option>Previous Customer</option>
+                                    <option>I am looking for a job</option>
 
-    <Button type="submit">Register New Account</Button>
+                                </Select>
+                            </div>
 
 
 
-</form>
 
-<div className="flex justify-center items-center mt-4">
-    <p>Have an account? <Login></Login> </p>
-    
-</div>
+                            <Button type="submit">Register New Account</Button>
 
-<Link to={'/payment'}><button className='btn- primary'>
-    PAY
-    </button></Link>
-</div>
+
+
+                        </form>
+
+                        <div className="flex justify-center items-center mt-4">
+                            <p>Have an account? <Login></Login> </p>
+
+                        </div>
+
+                    </div>
+                </div>
+
             </div>
-            
-        </div>
-       
+
         </>
     );
 };
