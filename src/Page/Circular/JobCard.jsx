dd2f14ff18480d975909af197dbf5b3e765469c7
Rahim00001 <rahimlpt2022@gmail.com>
2024-02-02 09:54:46 +0600

    test commit for pull request

--- src/Page/Circular/JobCard.jsx
+++ src/Page/Circular/JobCard.jsx
@@ -79,7 +79,7 @@ const JobCard = (jobs) => {
               <Link to={`/updateJob/${_id}`} className=" px-3 py-2 bg-blue-700 text-white  rounded-md hover:bg-slate-800 hover:-translate-y-[2px] duration-75 cursor-pointer flex items-center w-fit gap-2">Update<FaArrowRightLong></FaArrowRightLong> </Link>
               <Button onClick={() => handleDelete(_id)}>
                 {/* <HiShoppingCart className="mr-2 h-5 w-5" /> */}
-                Delete
+                Delete It
               </Button>
             </div>
           </div>
