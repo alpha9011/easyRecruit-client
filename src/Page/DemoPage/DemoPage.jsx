72abb5d86163fe4c3e185e6309c845b1ba93ad5b
fasunan <fasunan@gmail.com>
2024-02-20 02:25:15 +0600

    added demopage for new User to about ATS web

--- src/Page/DemoPage/DemoPage.jsx
+++ src/Page/DemoPage/DemoPage.jsx
@@ -1,9 +1,59 @@
+/* eslint-disable react/no-unescaped-entities */
 import Slider from "./Slider";
+import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
+import { SiQuora } from "react-icons/si";
+import Footers from "../../shared/Footer/Footers";
 
 const DemoPage = () => {
     return (
         <div>
-            <Slider></Slider>
+            <div>
+                <Slider></Slider>
+            </div>
+            <div className="grid grid-cols-1 lg:grid-cols-2">
+                <div className=" relative h-[600px] p-8">
+                    {/* Background Image */}
+                    <div
+                        className="absolute inset-0 bg-cover bg-center bg-no-repeat backdrop-filter "
+                        style={{
+                            backgroundImage: `url('https://i.ibb.co/0mx2DWy/bg2.jpg')`
+                        }}
+                    />
+                    {/* Client Review Section */}
+                    <div className="relative z-10 py-16 px-6 mt-24 lg:px-16 text-white text-center ">
+                        <div className="text-center ">
+                            <p className="flex gap-3 items-center justify-center"><FaQuoteLeft></FaQuoteLeft><SiQuora /><FaQuoteRight /></p>
+                            <h1 className="text-xl lg:text-3xl font-semibold mb-3 text-blue-400">"EasyRecruit has really helped us add consistency and clarity in our hiring process."</h1>
+                            <p className="text-xl">Devin Kearns | Staff Recruitment Manager</p>
+                            <p className="text-xl">North Carolina Outward Bound Logo</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="flex items-center justify-center p-8 text-center">
+                    <div className="mx-8">
+                        <h1 className="text-3xl font-bold mb-10">Schedule Time Now</h1>
+                        <div className="flex flex-col gap-8">
+                            <div className="flex items-center gap-4">
+                                <div className="w-1/2 border-r border-gray-300 pr-4">
+                                    <h2 className="text-xl font-bold mb-2">Exploring recruiting software for your company?</h2>
+                                    <p className="text-lg">I’m a business owner or HR team member looking to learn more about EasyRecruit</p>
+                                    <button className="mt-5 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Speak to an EasyRecruit Rep.</button>
+                                </div>
+                                <div className="w-1/2 pl-4">
+                                    <h2 className="text-xl font-bold mb-2">Do not have much Idea about Application Tracking system?</h2>
+                                    <p className="text-lg mb-12">Explore with EasyRecruit FAQ</p>
+                                    <button className="mt-5 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Explore with Us</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div className="mt-5">
+                <Footers></Footers>
+            </div>
         </div>
     );
 };
