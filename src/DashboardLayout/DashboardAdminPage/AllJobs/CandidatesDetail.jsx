5bd93c7f78e0c8784cbd8069d1a1ee97d8089dff
redwan islam <redwan390309@gmail.com>
2024-02-19 11:22:07 +0600

    update candidate details

--- src/DashboardLayout/DashboardAdminPage/AllJobs/CandidatesDetail.jsx
+++ src/DashboardLayout/DashboardAdminPage/AllJobs/CandidatesDetail.jsx
@@ -67,7 +67,7 @@ const handlenNOtSelect = id => {
                     Swal.fire({
                         position: "top-center",
                         icon: "success",
-                        title: `selected successfully`,
+                        title: `Not selected`,
                         showConfirmButton: false,
                         timer: 1500
                     });
@@ -82,35 +82,85 @@ const handlenNOtSelect = id => {
         <Table.Cell><button onClick={() => setOpenModal(true)} className="hover:text-blue-600 font-semibold">Details</button>
             <Modal show={openModal} onClose={() => setOpenModal(false)}>
                 <Modal.Header className="w-full">    <div className="">
-                  <div>
-                  <img src={photo} alt="candidate photo" className="w-24 "/>
-                   <h1>{name}</h1>
-                   <h4 className="text-sm">Apply Date: {applyDate}</h4>
-                  </div>
+                <div className="text-center ">
+          
+              
+              <img
+                src={photo}
+                alt="candidate photo"
+                className="absolute -top-12 left-0 right-0  w-24 h-24 rounded-full mx-auto hover:shadow-md hover:shadow-[#1D227C]"
+             
+              />
+
+
+            <h1 className="text-xl md:text-2xl font-semibold">{name}</h1>
+            <h4 className="text-sm">
+              <span className="font-semibold">Apply Date : </span>
+              {applyDate}
+            </h4>
+          </div>
                 
                 </div></Modal.Header>
 
                 <Modal.Body >
                    <div>
-                    <h2>Email:{email}</h2>
-                    <h2>Phone: {phone}</h2>
-                    <h3>Country: {country}</h3>
-                    <a href={resume} rel="noreferrer" className="text-blue-500" target="_blank">Resume</a>
-                    <a href={coverLetter} rel="noreferrer" className="text-blue-500" target="_blank">Cover Letter</a>
-                    <h3>Last Academy: {lastAcademy}</h3>
-                    <h3>Language: {language}</h3>
-                    <h3>Expected salary: ${salary}</h3>
-                    <h4>Gender: {gender}</h4>
+                   <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
+              <h2>
+                <span className="font-semibold">Email :</span> {email}
+              </h2>
+              <h2>
+                <span className="font-semibold"> Phone:</span> {phone}
+              </h2>
+              <h3>
+                <span className="font-semibold"> Country:</span> {country}
+              </h3>
 
-               <div className="mt-3">
+              <a
+                href={resume}
+                rel="noreferrer"
+                className="text-blue-500 hover:scale-105 duration-300 hover:translate-x-3"
+                target="_blank"
+                
+              >
+                Resume
+              </a>
+              <a
+                href={coverLetter}
+                rel="noreferrer"
+                className="text-blue-500 hover:scale-105 duration-300 hover:translate-x-3"
+                target="_blank"
+              >
+                Cover Letter
+              </a>
+              <h3>
+                <span className="font-semibold"> Last Academy: </span>{" "}
+                {lastAcademy}
+              </h3>
+              <h3>
+                <span className="font-semibold"> Language: </span> {language}
+              </h3>
+              <h3>
+                <span className="font-semibold"> Expected salary: </span> $
+                {salary}
+              </h3>
+              <h4>
+                <span className="font-semibold"> Gender: </span> {gender}
+              </h4>
+            </div>
+
+          <div className="flex items-center gap-5 mt-5">
+          <div className="">
                {
-                        isSelected === 'selected'? <span className="bg-green-400 px-4 py-2 rounded-md "> selected</span> :
+                        isSelected === 'selected'? <span className="bg-green-400 px-5 py-2 rounded-md "> selected</span> :
                         
-                    <button className="bg-blue-600 px-3 py-2 rounded-md text-white" onClick={()=>handleUpdateSelected(_id)}>Select</button>
+                    <button className="bg-blue-600 px-5 py-2 rounded-md text-white" onClick={()=>handleUpdateSelected(_id)}>Select</button>
                     }
 
                </div>
-               <button className="bg-red-600 px-3 py-2 rounded-md text-white mt-5" onClick={()=>handlenNOtSelect(_id)}>Not Select</button>
+               <div>
+               <button className="bg-red-600 px-3 py-2 rounded-md text-white " onClick={()=>handlenNOtSelect(_id)}>Not Select</button>
+               </div>
+          </div>
                    </div>
                 </Modal.Body>
 
