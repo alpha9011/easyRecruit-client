a8ac14e47385d288fbd688f5fe8cb7996f0fe9be
sazzadchy50 <sazzadchy50@gmail.com>
2024-02-22 21:37:09 +0600

    delete unwanted file

--- src/DashboardLayout/DashboardPage/Support/constants.js
+++
@@ -1,5 +0,0 @@
-export const APP_ID = 1391604880;
-export const SERVER_SECRET ="6a2b2e7cd3684ea89570527c0c349ab7";
-
-// export const APP_ID = import.meta.env.VITE_APP_ID;
-// export const SERVER_SECRET = import.meta.env.VITE_SERVER_SECRET;
