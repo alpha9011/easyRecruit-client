ef811cce65546e145417e5f00eb06a1931f11931
Rahim00001 <rahimlpt2022@gmail.com>
2024-02-26 23:56:00 +0600

    all button make clickable on about us, pricing and why us page

--- src/Page/Pricing/AllPrices.jsx
+++ src/Page/Pricing/AllPrices.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import useAxiosPublic from "../../Hooks/useAxiosPublic";
 import PriceCard from "./PriceCard";
+import { Link } from "react-router-dom";
 // import { GrFireball } from "react-icons/gr";
 
 
@@ -26,18 +27,22 @@ const AllPrices = () => {
             <div className="w-full flex flex-col justify-center items-center gap-5 my-16" data-aos="fade-up" data-aos-duration="1000">
                 <p className="text-2xl">Let’s transform how you hire.</p>
                 <div className="flex gap-3">
-                    <button
-                        type="button"
-                        className="inline-flex w-[150px] justify-center rounded bg-cyan-500 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-white hover:text-cyan-500 border-2 duration-500 border-cyan-500 focus:outline-none focus:ring-4 focus:ring-cyan-200 dark:focus:ring-cyan-900"
-                    >
-                        Request a Demo
-                    </button>
-                    <button
-                        type="button"
-                        className="inline-flex w-[150px] justify-center rounded hover:bg-cyan-500 px-5 py-2.5 text-center text-sm font-medium hover:text-white bg-white text-cyan-500 border-2 duration-500 border-cyan-500 focus:outline-none focus:ring-4 focus:ring-cyan-200 dark:focus:ring-cyan-900"
-                    >
-                        Try it Free
-                    </button>
+                    <Link to="/demoPage">
+                        <button
+                            type="button"
+                            className="inline-flex w-[150px] justify-center rounded bg-cyan-500 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-white hover:text-cyan-500 border-2 duration-500 border-cyan-500 focus:outline-none focus:ring-4 focus:ring-cyan-200 dark:focus:ring-cyan-900"
+                        >
+                            Request a Demo
+                        </button>
+                    </Link>
+                    <Link to="/register">
+                        <button
+                            type="button"
+                            className="inline-flex w-[150px] justify-center rounded hover:bg-cyan-500 px-5 py-2.5 text-center text-sm font-medium hover:text-white bg-white text-cyan-500 border-2 duration-500 border-cyan-500 focus:outline-none focus:ring-4 focus:ring-cyan-200 dark:focus:ring-cyan-900"
+                        >
+                            Try it Free
+                        </button>
+                    </Link>
                 </div>
             </div>
         </div>
